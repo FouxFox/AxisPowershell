4025c9b38678ec2bccb85bb5d7d593c8a19565c5
fouxfox <matt@frostisles.com>
2025-08-31 19:41:32 -0500

    Documentation fix for Get-AxisACAP

--- public/Get-AxisACAP.ps1
+++ public/Get-AxisACAP.ps1
@@ -1,22 +1,42 @@
+<#
+.SYNOPSIS
+Retrieves the list of installed Axis Camera Application Platform (ACAP) applications from a specified device.
+
+.DESCRIPTION
+The Get-AxisACAP function connects to an Axis device and retrieves the list of installed ACAP applications.
+
+.PARAMETER Device
+The hostname or IP address of the Axis device from which to retrieve the ACAP information. This parameter is mandatory.
+
+.OUTPUTS
+Returns a list of installed ACAP applications on the specified device.
+
+.EXAMPLES
+Get-AxisACAP -Device "192.168.0.100"
+
+Name              : objectanalytics
+ApplicationID     : 412806
+NiceName          : AXIS Object Analytics
+Vendor            : Axis Communications
+Version           : 1.20.36
+Status            : Stopped
+License           : None
+ConfigurationPage : local/objectanalytics/index.html
+VendorHomePage    : http://www.axis.com
+LicenseName       : available
+
+Name              : vmd
+ApplicationID     : 143440
+NiceName          : AXIS Video Motion Detection
+Vendor            : Axis Communications
+Version           : 4.5.13
+Status            : Stopped
+License           : None
+ConfigurationPage : local/vmd/config.html
+VendorHomePage    : http://www.axis.com
+LicenseName       : available
+#>
 function Get-AxisACAP {
-    <#
-    .SYNOPSIS
-    Retrieves information about Axis ACAP.
-
-    .DESCRIPTION
-    This function is used to retrieve details about Axis ACAP.
-
-    .PARAMETER ParameterName
-    Description of the parameter.
-
-    .EXAMPLE
-    Get-AxisACAP -ParameterName Value
-
-    .NOTES
-    Author: Your Name
-    Date: $(Get-Date -Format "yyyy-MM-dd")
-    #>
-
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
@@ -34,5 +54,4 @@ function Get-AxisACAP {
     }
 
     return $result.application
-    
 }
